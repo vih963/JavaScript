79fda5bc4f6e7328c96e53e08ab485a26039abb8
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-06-17 00:30:37 -0300

    condição

---
+++ jsgb/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 40;
+console.log(`a velocidade do seu carro é ${vel} km/h`);
+
+if (vel > 60) { // condiçaõ simples
+  console.log('Você ultrapassou a velocidade permitida, MULTADO!"');
+}
+
+console.log("Dirija com segurança use sempre cinto de segurança");
