d9971bbd1de32a0275840b5a7bc2235a9475deae
Vinícius Camargo <144703134+vih963@users.noreply.github.com>
2025-07-23 21:08:39 -0300

    JS

--- jsgb/Exercicios JS/exercicio07072025/script.js
+++ jsgb/Exercicios JS/exercicio07072025/script.js
@@ -1,6 +1,20 @@
-function carregar () {
-
-
-var img = window.document.getElementById('img')
-
+function carregar() {
+  var msg = window.document.getElementById("msg");
+  var img = window.document.getElementById("imagem");
+  var data = new Date();
+  //var hora = data.getHours()
+  var hora = 22
+  msg.innerHTML = `Agora são ${hora} horas`;
+  if (hora >= 0 && hora < 12) {
+    //bom dia
+    img.src = "manhaedit.png";
+    document.body.style.background = "#e2cd9f";
+  } else if (hora >= 12 && hora <= 18) {
+    //boa tarde
+    document.body.style.background = "#b9846f";
+    img.src = "tarde.png";
+  } else {
+    //boa noite
+    img.src = "noite.png";
+  }
 }
